1ad1a93890455242d2bc1e5c9c06534527d7c7ab
unknown <kay@tuxlin-win.(none)>
2012-01-02 13:16:07 +0100

    add the demo Page

---
+++ js/fireRocket.js
@@ -0,0 +1,107 @@
+/**
+ * the fireRocket bundles x-numbers of particles to
+ * one rocket on the cloud!
+ */
+var fireRocket = function () {
+    this.particlesAnz = 3200;
+    this.i = 0;
+    this.color = this.getRandomColor();
+    this.particleObj = new Array();
+    this.startPositionX = ( Math.random() * (600 - 300 + 1) + 300);
+    this.startPositionY = ( Math.random() * (600 - 300 + 1) + 300);
+    this.startAlpha = 10;
+    this.r =  Math.random() * (20 - 1 + 1) + 1;
+}
+
+/**
+ * returns an random color 
+ *
+ */
+fireRocket.prototype.getRandomColor = function () {
+    var colors = new Array('red', 'purple','silver','yellow', 'pink', 'blue', 'white');
+    max = colors.length;
+    var op =  Math.floor(Math.random() * (max - 0 + 1)) + 0;
+    return colors[op];
+}
+
+
+/**
+ * now set all the start Points to the center of the canvas
+ */
+fireRocket.prototype.startRocket = function () {
+    for(x = 0; x <= this.particlesAnz;x++) {
+        this.createNewParticle(); 
+    }
+    this.sendToPaint();
+  
+}
+
+/**
+ * returns an random operator with an 50% chance for every 
+ * item
+ */
+fireRocket.prototype.getRandomOperator = function ( ) {
+    var op =  Math.floor(Math.random() * (1 - 0 + 1)) + 0;
+   
+    if(op == 1) {
+        return '+';
+    } 
+    if(op == 0) {
+        return '-';
+    } 
+}
+
+fireRocket.prototype.getColorParticle = function (particle) {
+    var op =  Math.floor(Math.random() * (1 - 0 + 1)) + 0;
+   
+    if(op == 0 || op == 2) {
+        return this.color;
+    } 
+    if(op == 1) {
+        return "#E2FF00";
+    } 
+}
+/**
+ * creates an new particle of the rocket 
+ * and sets some random params
+ */
+fireRocket.prototype.createNewParticle = function () {
+    particle = new particel();
+    particle.setLifetime(Math.floor(Math.random() * (20 - 5 + 1)) + 5);
+    particle.setPosition((this.r*Math.cos(this.startAlpha) ) +  this.startPositionX, (this.r*Math.sin(this.startAlpha ) ) + this.startPositionY);
+    particle.setAlpha(this.startAlpha);
+    particle.setRadius(this.r);
+    this.startAlpha++;
+    if(this.startAlpha == 380) {
+        this.startAplha = 0;
+    }
+    var argument = new Array();
+    argument.x= this.getRandomOperator();
+    argument.y = this.getRandomOperator();
+    particle.setMultiplicator( argument );
+    var particelColor = this.getColorParticle(particle);
+    
+    particle.setColor(particelColor);
+    particle.setSpeed( Math.random() * (10 - 2 + 1) + 2);
+    particle.setSpeedX( Math.random() * (10 - 2 + 1) + 2  );
+    if(particelColor == '#E2FF00') {
+       particle.setLifetime(3);
+       particle.setSpeed(1);
+    }
+    this.registerParticle(particle);
+
+}
+
+/**
+ * sends all the particle to the canvas Painter
+ */
+fireRocket.prototype.sendToPaint = function () {
+    painter.registerSet(this.particleObj);
+}
+
+/**
+ * register the particle here in the rocket
+ */
+fireRocket.prototype.registerParticle = function (particleObj) {
+    this.particleObj.push(particleObj);
+}
---
+++ js/particel.js
@@ -0,0 +1,112 @@
+var particel = function () {
+    this.setPosition; //set the new position
+    this.positions = new Array(); //the position in pixel x,y array
+    this.positions['x'] = 0;
+    this.positions['y'] = 0;
+    this.speed; //the speed in pixel per second
+    this.directionS; //the direction in grad
+    this.color; // here is the color of this pixel
+    this.lifetime; //the lifetime in seconds of this particel
+    this.size = 1;
+    this.newElementsPerRound;
+    this.speedx;
+    this.multiplikatorX;
+    this.multiplikatorY;
+    this.alpha;
+    this.radius;
+}
+        
+/**
+* set the new position
+* x, and y, use direction and speed to calc the new
+* position
+*/
+
+/**
+* sets the speed factor of this particel
+*/
+particel.prototype.setDirecton = function (direction) {
+    this.directionS = direction;
+}
+        
+/**
+* sets the new color
+*/
+particel.prototype.setColor = function (color) {
+    this.color = color;
+}
+        
+        
+/**
+* this must be called after set speed and direction is called
+*/
+particel.prototype.setPosition = function (x,y) {
+    this.positions.x = x;
+    this.positions.y = y;
+}
+        
+/**
+* sets the lifetime of this particel in paintCirculates or seconds ?
+* in seconds
+*/
+particel.prototype.setLifetime = function (lifetime) {
+    this.lifetime = lifetime;
+} 
+
+particel.prototype.setSpeed = function (speed) {
+    this.speed = speed;
+}
+
+/**
+ * sets the speed for the x coordinate
+ */
+particel.prototype.setSpeedX = function (speed) {
+    this.speedx = speed;
+}
+
+/**
+ * this is an easy way to set  the direction for the
+ * x and y coordinates
+ */
+particel.prototype.setAlpha = function (alpha) {
+    this.alpha = alpha;
+}
+
+particel.prototype.setRadius = function (radius) {
+    this.radius = radius;
+}
+
+/**
+ * this is an easy way to set  the direction for the
+ * x and y coordinates
+ */
+particel.prototype.setMultiplicator = function (multiplicator) {
+    this.multiplikatorX = multiplicator.x;
+    this.multiplikatorY = multiplicator.y;
+}
+        
+/**
+ * this mehtod calculates the new position
+ */
+particel.prototype.calcNewPosition = function () {
+    //calc here the new position!!
+    this.positions.x = (this.speed*Math.cos(this.alpha) ) +  this.positions.x,
+    this.positions.y = (this.speed*Math.sin(this.alpha ) ) + this.positions.y;
+}
+    
+    
+/**
+* this method draws itself to an given canvas 2d context
+*/
+particel.prototype.getPaintInformation = function (context) {
+    this.lifetime = this.lifetime  - 1;
+    if(this.lifetime <= 0) {
+        return false;
+    } else {
+        context.beginPath();  
+        context.strokeStyle = this.color;
+        context.strokeRect(this.positions.x,this.positions.y,this.size,this.size); // Outer circle
+        this.calcNewPosition();
+        return context;
+    }
+} 
